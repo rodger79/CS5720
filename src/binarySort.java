4e5f3455a0c038ef17039b10d5319dded16e5dec
Rodger Byrd <rodger.byrd@gmail.com>
2017-09-20 16:34:52 -0600

    final program

--- src/binarySort.java
+++ src/binarySort.java
@@ -1,3 +1,10 @@
+/*
+ * Rodger Byrd
+ * CS 5720 HW1
+ * 9/20/17
+ * 
+ */
+
 import java.io.*;
 import java.text.*;
 import java.util.*;
@@ -7,42 +14,41 @@ public class binarySort {
 	public static void main(String[] args) {
 		//report time
 		long startTime = System.currentTimeMillis();
-		int arraySize = 10;
+		int arraySize = 50000;
 		
-		//create array of random numbers from 0-100000
+		//create array of random sorted numbers 
 		Random rand = new Random();
 		int[] sortedArray = new int[arraySize];
 		int current = 0;
-		for (int i = 0; i < arraySize-1; i++){
-				sortedArray[i] = i-1;
-		}
-		/*
-		for (int i = 1; i < 999999; i++){
-			current = current + rand.nextInt(5);
-			if (i == 0){
+		int value = 0;
+
+		for (int i = 1; i < arraySize; i++){
+			current = current + rand.nextInt(10);
+			/*if (i == 0){
 				sortedArray[0]= 0;
 			}else{
-				sortedArray[i] = current + 1;
-			}
-			current = sortedArray[i];
-		}*/
-		
-		/*
-		for (int i = 0; i < 1999; i++){
-			System.out.println(sortedArray[i]);
+				
+			}*/
+			sortedArray[i] = current;
 		}
-		*/
+		
+		
+		//for (int i = 0; i < arraySize; i++){
+		//	System.out.println(sortedArray[i]);
+		//}
+		
 		//implement binary search algorithm
 		int key = rand.nextInt(arraySize);
 		int l = 0; //low index
 		int h = arraySize-1; //high index
 		int m; //median
-		int a; //index of match
+		int a = -1; //index of match
 		
 		while (l <= h){
 			m = (l + h)/2;
 			if (sortedArray[m]==key){
 				a = m;
+				break;
 			}
 			else if (key > sortedArray[m]){
 				l = m + 1;
@@ -50,9 +56,10 @@ public class binarySort {
 				h = m - 1;
 			}
 		}
-		a  = -1;
+		//print output to console including whether match was found and total time
 		System.out.println("Matching index = "+ a);
 		System.out.println("Key = "+ key);
+		System.out.println("Array Length = "+ arraySize);
 
 		long endTime   = System.currentTimeMillis();
 		long totalTime = endTime - startTime;
